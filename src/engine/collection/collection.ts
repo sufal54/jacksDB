fd12b79dfbe308d5243eacc43ac71e9462efbf4f
sufal54 <sufalbala%@gmail.com>
2025-07-01 06:09:47 +0530

    format code

--- src/engine/collection/collection.ts
+++ src/engine/collection/collection.ts
@@ -54,3 +54,4 @@ export class Collection {
                             if (fieldVal === val) {
                                 return false;
                             }
+                            break;
