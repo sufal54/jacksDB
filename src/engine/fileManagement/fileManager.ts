5576039b06c30c2ee83544330a588a14cf419dba
Sufal Bala <145686201+sufal54@users.noreply.github.com>
2025-07-03 11:19:22 +0000

    fix typo in docs

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -1,1 +1,11 @@
 import fs from "node:fs";
+import fsp from "node:fs/promises";
+import path from "node:path";
+import RwLock from "@sufalctl/rwlock";
+import Crypto from "../../crypto/crypto";
+import { IndexEntry, IndexOut, FindOptions } from "./types";
+
+export class FileManager {
+    private dataBasePath = "JacksDB";
+    private mainDB: string = "main.db.bson";
+    private fileLocks: Map<string, RwLock<void>> = new Map();
