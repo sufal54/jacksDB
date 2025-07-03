efd51f7ca3c075c8e6d260f32ae285b2e918d353
Sufal Bala <145686201+sufal54@users.noreply.github.com>
2025-07-03 11:19:22 +0000

    add semantic release

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -769,3 +769,13 @@ export class FileManager {
        */
 
     async makeAsDeleteAddNew(fileName: string, offset: number, doc?: Partial<IndexEntry>) {
+        const [_, rel] = await this.getLock(fileName).write();
+        const filePath = path.join(this.dataBasePath, fileName);
+        const write = await fsp.open(filePath, "r+");
+
+
+        const deletBufferMark = Buffer.alloc(1);
+        deletBufferMark.writeUInt8(0xDE);
+        await write.write(deletBufferMark, 0, 1, offset);
+        await write.sync();
+        await write.close();
