a350ca26b4ef1291f871a9f4bafe30417c6ca2c5
sufal54 <sufalbala%@gmail.com>
2025-07-22 09:22:56 +0530

    refactor code structure

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -830,3 +830,4 @@ export class FileManager {
                                     writeStream.destroy();
                                     readStream.destroy();
                                     rel();
+                                    return reject(err);
