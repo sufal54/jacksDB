e6589fd6a96aa56294267851f690c7277bd619fe
sufal54 <sufalbala%@gmail.com>
2025-07-22 09:22:56 +0530

    refactor code structure

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -171,3 +171,4 @@ export class FileManager {
 
             // Read the full block
             const fullBuffer = Buffer.alloc(totalSize);
+            await file.read(fullBuffer, 0, totalSize, offset);
