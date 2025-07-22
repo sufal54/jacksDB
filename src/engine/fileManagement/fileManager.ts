18e3b6f19d5f7872fdae2dada4fb8a8671e9270d
sufal54 <sufalbala%@gmail.com>
2025-07-22 09:22:56 +0530

    add helper function

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -394,3 +394,4 @@ export class FileManager {
         const flatDocs: Partial<IndexEntry>[] = docs.flat(); // Remove nestet array
 
         const [_, rel] = await this.getLock(fileName).write();
+        const fullPath = path.join(this.dataBasePath, fileName);
