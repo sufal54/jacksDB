6d7f3b72fcc68177e4d8f4c46742ba36e6a786f5
sufal54 <sufalbala%@gmail.com>
2025-07-22 09:22:56 +0530

    format code

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -681,3 +681,4 @@ export class FileManager {
             const encodeDoc = this.crypto.encrypt(JSON.stringify(newData));
 
             if (idxData.capacity < encodeDoc.readUInt32LE(1)) {
+                await this.makeAsDeleteAddNew(fileName, idxData.offset, newData);
