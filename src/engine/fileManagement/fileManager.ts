dc0b0b4d0d4f00528827dff32bdb9be90af387ea
sufal54 <sufalbala%@gmail.com>
2025-07-22 09:22:56 +0530

    fix typo in docs

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -688,3 +688,4 @@ export class FileManager {
             const capacityBuffer = Buffer.alloc(4);
             capacityBuffer.writeInt32LE(idxData.capacity);
             capacityBuffer.copy(encodeDoc, 5, 0, 4);
+
