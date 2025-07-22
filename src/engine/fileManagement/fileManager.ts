6f7faadd95c596638353b2a5225fae8e47156de3
sufal54 <sufalbala%@gmail.com>
2025-07-22 09:22:56 +0530

    optimize function

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -607,3 +607,4 @@ export class FileManager {
     private async indexField(key: string, val: string | number | boolean, offset: number) {
         const file = `${key}.idx.bson`;
         // Ensure fiel exist or create new
+        this.ensureFile(file);
