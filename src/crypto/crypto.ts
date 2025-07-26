c158bb0891c59e3ff1caee8286bca5f6fbd15410
Sufal Bala <145686201+sufal54@users.noreply.github.com>
2025-07-26 09:59:37 +0000

    improve security headers

--- src/crypto/crypto.ts
+++ src/crypto/crypto.ts
@@ -1,1 +1,11 @@
 import * as crypto from 'node:crypto';
+import * as os from 'node:os';
+
+class Crypto {
+    private readonly algorithm = "aes-256-cbc"; // AES algorithm
+    private readonly key: Buffer;
+
+
+    constructor(secretKey?: string) {
+        // Generate a default key using OS hostname and platform if not provided
+        const finalKey = secretKey || `${os.hostname()}-${os.platform()}`;
