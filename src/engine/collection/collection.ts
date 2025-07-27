116f64df845f90ef9a979f6ec746ff8fe69058e0
sufal54 <sufalbala%@gmail.com>
2025-07-28 03:24:49 +0530

    refactor code structure

--- src/engine/collection/collection.ts
+++ src/engine/collection/collection.ts
@@ -72,3 +72,4 @@ export class Collection {
                             break;
                         case "$lte":
                             if (typeof fieldVal !== "number" || typeof val !== "number" || !(fieldVal <= val)) {
+                                return false;
