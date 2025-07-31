e1c185862bd815ca4f6db76f3d98d09218e705c6
sufal54 <sufalbala%@gmail.com>
2025-08-01 02:41:56 +0530

    update config settings

--- src/engine/collection/collection.ts
+++ src/engine/collection/collection.ts
@@ -338,3 +338,4 @@ export class Collection {
 
     async updateMany(filter: Record<string, any>, update: Partial<any>): Promise<void> {
         const found = await this.find(filter);
+        const updateParsed = this.dotPathToObject(update);
