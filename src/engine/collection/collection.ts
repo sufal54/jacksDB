d6e69be6bbc8c0853399138929d58d9a4693545b
sufal54 <sufalbala%@gmail.com>
2025-08-01 02:41:56 +0530

    minor bug fix

--- src/engine/collection/collection.ts
+++ src/engine/collection/collection.ts
@@ -339,3 +339,4 @@ export class Collection {
     async updateMany(filter: Record<string, any>, update: Partial<any>): Promise<void> {
         const found = await this.find(filter);
         const updateParsed = this.dotPathToObject(update);
+
