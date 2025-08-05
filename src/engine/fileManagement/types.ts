5111bcfa9cda92c7020b0caa144c8cabe46c4393
sufal54 <sufalbala%@gmail.com>
2025-08-05 22:04:27 +0530

    optimize function

--- src/engine/fileManagement/types.ts
+++ src/engine/fileManagement/types.ts
@@ -1,16 +1,1 @@
 export type IndexEntry = {
-    offset?: number;
-    [key: string]: any;
-};
-
-export type IndexOut = {
-    offset: number;
-    length: number;
-    capacity: number;
-} & Record<string, number[]>;
-
-export type FindOptions = {
-    sort?: Record<string, 1 | -1>;
-    skip?: number;
-    limit?: number;
-}
