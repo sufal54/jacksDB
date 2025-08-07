ce34e9f1828aba5e8a43a1fd7734bab60c2648a6
Sufal Bala <145686201+sufal54@users.noreply.github.com>
2025-08-07 06:56:28 +0000

    add theme support

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -9,3 +9,13 @@ export class FileManager {
     private dataBasePath = "JacksDB";
     private mainDB: string = "main.db.bson";
     private fileLocks: Map<string, RwLock<void>> = new Map();
+    private crypto: Crypto;
+
+
+    private _index: Map<string, Map<string, Set<number>>> = new Map(); // test no use
+    private _fieldIndexes: Record<string, Map<string, number | number[]>> = {}; // test no use
+
+    /**
+    Initialize FileManager of a collection
+    * @param {string} name - name of collection
+    * @param {String} secret - option a secret key for better encryption
