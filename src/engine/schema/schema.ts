b9440a94709de4a467f799982541dcf6964c2719
sufal54 <sufalbala%@gmail.com>
2025-08-08 13:39:16 +0530

    update readme

--- src/engine/schema/schema.ts
+++ src/engine/schema/schema.ts
@@ -4,3 +4,4 @@ export type SchemaDefinition = {
     | NumberConstructor
     | BooleanConstructor
     | SchemaDefinition
+    | [StringConstructor | NumberConstructor | BooleanConstructor | SchemaDefinition];
