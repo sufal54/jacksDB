e5ffc52b4fd336be885c8365e2c6d5e7f3802233
sufal54 <sufalbala%@gmail.com>
2025-08-09 20:49:59 +0530

    fix typo in docs

--- src/engine/collection/collection.ts
+++ src/engine/collection/collection.ts
@@ -98,3 +98,4 @@ export class Collection {
                             console.warn(`Unsupported operator: ${op}`);
                             return false;
                     }
+                }
