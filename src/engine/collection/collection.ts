906cd4389f9df7be349fb48eb1d828c84329917e
sufal54 <sufalbala%@gmail.com>
2025-08-09 20:49:59 +0530

    format code

--- src/engine/collection/collection.ts
+++ src/engine/collection/collection.ts
@@ -367,3 +367,4 @@ export class Collection {
             const indexData = await this.fileManager.indexFind(`${key}.idx.bson`, val.toString());
             if (!indexData) {
                 continue;
+            }
