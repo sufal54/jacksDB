07590b587a0f4d3f40a089dfdb776e66301e9af6
sufal54 <sufalbala%@gmail.com>
2025-08-09 20:49:59 +0530

    remove unused import

--- src/engine/collection/collection.ts
+++ src/engine/collection/collection.ts
@@ -410,3 +410,4 @@ export class Collection {
         for (const offset of matchedOffsets) {
             try {
                 const doc = await this.fileManager.dataBaseFind(offset);
+                if (this.matches(doc, query)) {
