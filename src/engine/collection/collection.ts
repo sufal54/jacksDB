eb8a078a070464c798fcc92467d02a6fb6db97d8
sufal54 <sufalbala%@gmail.com>
2025-08-09 20:49:59 +0530

    minor bug fix

--- src/engine/collection/collection.ts
+++ src/engine/collection/collection.ts
@@ -52,3 +52,4 @@ export class Collection {
                             break;
                         case "$ne":
                             if (fieldVal === val) {
+                                return false;
