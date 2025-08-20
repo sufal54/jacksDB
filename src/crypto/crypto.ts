bd25ffd1020208f5f39e06b5c5ba0e3f8d20ecc4
Sufal Bala <145686201+sufal54@users.noreply.github.com>
2025-08-20 20:18:54 +0000

    update dependency lock

--- src/crypto/crypto.ts
+++ src/crypto/crypto.ts
@@ -9,3 +9,13 @@ class Crypto {
     constructor(secretKey?: string) {
         // Generate a default key using OS hostname and platform if not provided
         const finalKey = secretKey || `${os.hostname()}-${os.platform()}`;
+
+        this.key = crypto.createHash("sha256").update(finalKey).digest();
+    }
+
+    /**
+     * parse jason data to encrypted buffer 
+     * including 0xfd 4byte length 4byte of capcity 16byte of iv and rest of data + 50byte of extra length
+     * @param text - json string
+     * @returns - encrypted buffer
+     */
