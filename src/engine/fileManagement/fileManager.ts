b65f94dd9d9a9e3b335612b44d7bda88bdbac43d
sufal54 <sufalbala%@gmail.com>
2025-08-22 09:42:47 +0530

    update readme

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -419,3 +419,4 @@ export class FileManager {
                 offset += 1 + 4 + 4 + 16 + capacity;
 
                 // Send all key value for nested value 
+                for (const [key, value] of Object.entries(doc)) {
