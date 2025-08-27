4ac112585ef751ed2a4bd31120e761c33a22d542
Sufal Bala <145686201+sufal54@users.noreply.github.com>
2025-08-27 16:17:34 +0000

    format code

--- src/engine/collection/collection.ts
+++ src/engine/collection/collection.ts
@@ -1,1 +1,11 @@
 import { FileManager } from "../fileManagement/fileManager";
+import Schema from "../schema/schema";
+
+export class Collection {
+    private fileManager: FileManager;
+    private schema: Schema;
+
+    constructor(collectionName: string, schema: Schema, secret?: string) {
+        this.schema = schema;
+        this.fileManager = new FileManager(collectionName, secret);
+    }
