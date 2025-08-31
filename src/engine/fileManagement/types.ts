380aedf4b800ffd4c4a3ab5524c12986b222018f
Sufal Bala <145686201+sufal54@users.noreply.github.com>
2025-08-31 14:58:05 +0000

    fix sorting issue

--- src/engine/fileManagement/types.ts
+++ src/engine/fileManagement/types.ts
@@ -1,11 +1,1 @@
 export type IndexEntry = {
-    offset?: number;
-    [key: string]: any;
-};
-
-export type IndexOut = {
-    offset: number;
-    length: number;
-    capacity: number;
-} & Record<string, number[]>;
-
