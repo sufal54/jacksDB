94fcbe9e7fa394967cf29b3b32b681e729d06321
Sufal Bala <145686201+sufal54@users.noreply.github.com>
2025-08-31 14:58:05 +0000

    update prod config

--- src/engine/fileManagement/types.ts
+++ src/engine/fileManagement/types.ts
@@ -1,1 +1,11 @@
 export type IndexEntry = {
+    offset?: number;
+    [key: string]: any;
+};
+
+export type IndexOut = {
+    offset: number;
+    length: number;
+    capacity: number;
+} & Record<string, number[]>;
+
