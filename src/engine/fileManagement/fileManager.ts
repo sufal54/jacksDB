f31ba409c70c469562f632eef204372ed153fdef
sufal54 <sufalbala%@gmail.com>
2025-09-05 01:12:39 +0530

    remove unused import

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -752,3 +752,4 @@ export class FileManager {
             const capacityBuffer = Buffer.alloc(4);
             capacityBuffer.writeInt32LE(idxData.capacity);
             capacityBuffer.copy(encodeDoc, 5, 0, 4);
+            await fileHandle.write(encodeDoc, 0, encodeDoc.length, idxData.offset);
