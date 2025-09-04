337d0dfe8f871243ad6771a40b13c4043d67bb44
sufal54 <sufalbala%@gmail.com>
2025-09-05 01:12:39 +0530

    format code

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -683,3 +683,4 @@ export class FileManager {
             if (idxData.capacity < encodeDoc.readUInt32LE(1)) {
                 await this.makeAsDeleteAddNew(fileName, idxData.offset, newData);
                 return;
+            }
