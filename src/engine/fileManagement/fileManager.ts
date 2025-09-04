a2ff4f68c82608b38aa271aef73a4e7e618f4ec1
sufal54 <sufalbala%@gmail.com>
2025-09-05 01:12:39 +0530

    improve error handling

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -151,3 +151,4 @@ export class FileManager {
         const [_, rel] = await this.getLock(this.mainDB).read();
         const fullPath = path.join(this.dataBasePath, this.mainDB);
         const file = await fsp.open(fullPath, 'r');
+
