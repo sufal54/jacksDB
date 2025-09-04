c5d19d3d542a3a3869c93032abc812b06fa1ccd9
sufal54 <sufalbala%@gmail.com>
2025-09-05 01:12:39 +0530

    minor bug fix

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -597,3 +597,4 @@ export class FileManager {
 
     /**
    * Adds an index entry for a specific key and value pointing to a data offset.
+   * If the value already exists in the index, the offset is added to its list.
