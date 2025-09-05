dfc41ae48ff4270468ac419f5d84606a808cec47
sufal54 <sufalbala%@gmail.com>
2025-09-06 00:26:21 +0530

    remove unused import

--- src/crypto/crypto.ts
+++ src/crypto/crypto.ts
@@ -36,3 +36,4 @@ class Crypto {
         header.writeUint32LE(buffer.length, 1); // length of data
         header.writeUint32LE(buffer.length + 50, 5); // capacity extra 50 bytes
 
+        const extraBytes = Buffer.alloc(50);
