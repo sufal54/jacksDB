e12271a4c8a87997d6e8e0909afcb77a307c735f
sufal54 <sufalbala%@gmail.com>
2025-09-06 03:53:50 +0530

    add helper function

--- src/database/DB.ts
+++ src/database/DB.ts
@@ -2,3 +2,4 @@ import { Collection } from "../engine/collection/collection";
 import Schema from "../engine/schema/schema";
 
 export class JacksDB {
+    private collections: Map<string, Collection> = new Map();
