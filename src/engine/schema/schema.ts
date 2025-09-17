14e515eaf1d02ef677b6c70fcb8cb24d0d083446
sufal54 <sufalbala%@gmail.com>
2025-09-17 08:11:54 +0530

    remove unused import

--- src/engine/schema/schema.ts
+++ src/engine/schema/schema.ts
@@ -27,3 +27,4 @@ class Schema {
             } else if (this.isPlainObject(value)) {
                 // Nested object schema
                 this.definition[key] = new Schema(value as SchemaDefinition);
+            } else {
