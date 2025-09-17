e17b180f73367ed7e6e4c20617f7aed0bc281f97
sufal54 <sufalbala%@gmail.com>
2025-09-17 08:39:37 +0530

    update config settings

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -632,3 +632,4 @@ export class FileManager {
         const [_, rel] = await this.getLock(fileName).write();
         const fullPath = path.join(this.dataBasePath, fileName);
         const write = await fsp.open(fullPath, "a");
+        try {
