f9e422d9dbe5e130c08b0ba0957b4a7bcce4a46e
sufal54 <sufalbala%@gmail.com>
2025-09-17 08:39:37 +0530

    update config settings

--- src/engine/fileManagement/fileManager.ts
+++ src/engine/fileManagement/fileManager.ts
@@ -391,3 +391,4 @@ export class FileManager {
 
     async dataBaseInsert(fileName: string, ...docs: Partial<IndexEntry>[]): Promise<void> {
 
+        const flatDocs: Partial<IndexEntry>[] = docs.flat(); // Remove nestet array
